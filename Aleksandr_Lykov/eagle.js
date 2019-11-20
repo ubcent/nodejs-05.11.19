956da70c93f27f2eca5a41655a4051c8e35291cf
Aleks <aleks.lykov@gmail.com>
2019-11-20 17:41:39 +0300

    Сделал анализ логов

--- Aleksandr_Lykov/eagle.js
+++ Aleksandr_Lykov/eagle.js
@@ -12,23 +12,23 @@ const rl = readline.createInterface({
     output: process.stdout,
 })
 
-fs.closeSync(fs.openSync(path.normalize(`./Aleksandr_Lykov/${argv.f}`), 'w+'))
+if(!argv.f) argv.f = 'log.txt'
+fs.closeSync(fs.openSync(`./Aleksandr_Lykov/${argv.f}`, 'a+'))
 
 const question = (i) => {
-    return new Promise( res => rl.question(`What's your choice (Орел - 0 or Решка - 1)? - `, (choice) => {
+    return new Promise( res => rl.question(`What's your choice (Орел - 0 or Решка - 1)? - `, choice => {
         let coin = Math.floor(Math.random() * 2);
         fs.appendFile(path.normalize(`./Aleksandr_Lykov/${argv.f}`),`Move ${i}. Your choice: ${choice}.\n  Coin throw result: ${coin}\n\n`, err => { return })
         if(coin == choice)  { console.log('Вы выиграли!!!')
             fs.appendFile(path.normalize(`./Aleksandr_Lykov/${argv.f}`),`Number of attempts: ${i}\n----------------\n\n`, err => { return })
             res(true) }
             else { console.log('Вы проиграли!!!'); res(false) }
-    }))
-}
+}))}
 
 const main_questions = async () => {
     for(let i=1; ; i++) {
-    let answer = await question(i)
-    if(answer) { rl.close(); return }
+        let answer = await question(i)
+        if(answer) { rl.close(); return }
     }
 }
 main_questions()
