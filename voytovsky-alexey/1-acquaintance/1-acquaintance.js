b0a1af7098fa4188d8c0f384e08bd63c8004a5bf
Alexey Voytovsky <al.voytovsky@gmail.com>
2019-12-02 09:23:06 +0300

    2 пробела вместо 4х.

--- voytovsky-alexey/1-acquaintance/1-acquaintance.js
+++ voytovsky-alexey/1-acquaintance/1-acquaintance.js
@@ -1,10 +1,13 @@
-const { red, bold, reset } = require("cli-color");
+const { red, bold } = require("cli-color");
 const { radar } = require('chalk-animation');
- 
-console.log(red(bold.italic.underline('Acquaintance with Node.js')));
-radar('-------------------------', 1);
 
-setTimeout(() => {
-    // process.stdout.write(reset);
-    process.exit(0);
-}, 3000);
+const program = () => { 
+	console.log(red(bold.italic.underline('Acquaintance with Node.js')));
+	radar('-------------------------', 1);
+
+	setTimeout(() => {
+		process.exit(0);
+	}, 3000);
+};
+
+program();
