745e6cbee7d9bc2b6c02a74e102fb8d65eee95f9
Dmitry Bondarchuk <dvbondarchuk@gmail.com>
2019-12-24 21:54:04 +0300

    Lesson 8

--- rest.js
+++ rest.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 mongoose.connect('mongodb://localhost:32768/tasks', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
@@ -10,6 +12,48 @@ const Task = require('./models/task');
 const User = require('./models/user');
 
 const app = express();
+
+const server = http.Server(app);
+const io = socketIO(server);
+
+io.use((socket, next) => {
+  const token = socket.handshake.query.token;
+
+  jwt.verify(token, 'super secret key', (err) => {
+    if (err) {
+      return next(new Error('authentication error'));
+    }
+
+    next();
+  });
+
+  return next(new Error('authentication error'));
+});
+
+io.on('connection', (socket) => {
+  console.log('Someone has connected!');
+
+  socket.on('create', async (data) => {
+    const task = new Task(data);
+    const savedTask = await task.save();
+
+    socket.broadcast.emit(`created:${savedTask.user}`, savedTask);
+    socket.emit(`created:${savedTask.user}`, savedTask);
+  });
+
+  socket.on('toggle', async (taskId) => {
+    const task = await Task.findById(taskId);
+
+    await Task.findOneAndUpdate({ _id: taskId }, { $set: { completed: !task.completed } });
+
+    socket.broadcast.emit(`toggled:${task.user}`, taskId);
+    socket.emit(`toggled:${task.user}`, taskId);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Someone has disconnected!');
+  });
+});
 
 app.use(express.json());
 app.use(cors());
@@ -50,7 +94,7 @@ app.use('/tasks', checkAuthentication);
 app.get('/tasks', async (req, res) => {
   const { page = 1, limit = 10 } = req.query;
 
-  const tasks = await Task.find().skip((page - 1) * limit).limit(limit);
+  const tasks = await Task.find({ user: req.user._id }).skip((page - 1) * limit).limit(limit);
 
   res.status(200).json(tasks);
 });
@@ -62,7 +106,7 @@ app.get('/tasks/:id', async (req, res) => {
 });
 
 app.post('/tasks', (req, res) => {
-  const task = new Task(req.body);
+  const task = new Task({ ...req.body, user: req.user._id });
 
   task.save()
     .then((savedTask) => {
@@ -82,7 +126,7 @@ app.put('/tasks/:id', async (req, res) => {
 app.patch('/tasks/:id', async (req, res) => {
   const task = await Task.findById(req.params.id).lean();
 
-  const modifiedTask = await Task.findOneAndUpdate({ _id: req.params.id }, { $set: {...task, ...req.body} });
+  const modifiedTask = await Task.findOneAndUpdate({ _id: req.params.id }, { $set: { ...task, ...req.body } });
 
   res.status(200).json(modifiedTask);
 });
@@ -129,6 +173,6 @@ app.post('/auth', async (req, res) => {
 });
 
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log('Server has started!');
 });
