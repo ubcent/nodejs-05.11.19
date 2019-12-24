7e89a8cdca4e7f8f1234e029ac4940ea13f0948b
konstantin141091 <konstantin.sudakov@bk.ru>
2019-12-24 21:29:49 +0900

    делаю дз7 rest api. Начал переписыать api. Осталось редактирование. Аунт и рег не делал

--- Konstantin_Sudakov/lesson5_and_6/index.js
+++ Konstantin_Sudakov/lesson5_and_6/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const consolidate = require('consolidate');
+const cors = require('cors');
 const path = require('path');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const config = require('./config.json');
+const jwt = require('jsonwebtoken');
 
 // здесь подключаю конфиг с настройками подключения к mongodb
 mongoose.connect(config.mogodbConfig, {useNewUrlParser: true, useUnifiedTopology: true});
@@ -28,53 +30,66 @@ app.use(session({
 app.use(passport.initialize);
 app.use(passport.session);
 app.use('/tasks', passport.mustBeAuthenticated);
-app.use('/task', passport.mustBeAuthenticated);
+app.use(cors());
+app.use(express.json());
+// app.use(body_parser);
 
 //главная страница
 app.get('/', (req, res) => {
-  res.render('main');
+  res.status(200).render('main');
 });
 // список всех задач
 app.get('/tasks', async (req, res) => {
   const {_id} = req.user;
   const tasks = await Task.find({user: _id});
-  res.render('tasks', {tasks});
+  if (tasks) {
+    res.status(200).render('tasks', {tasks});
+  } else {
+    res.status( 404 ).send();
+  }
 });
 //отметить выполнение
-app.post('/tasks/completed', async (req, res) => {
-  let completed = true;
-  if (req.body.completed === 'Отметить как невыполненую') {
-    completed = false;
-  }
-  await Task.updateOne({_id: req.body._id}, {completed: completed});
-  res.redirect('/tasks');
+app.put('/completed/:id/:completed', async (req, res) => {
+
+  console.log(req.params.id);
+  console.log(req.params.completed);
+  // console.log(req.body);
+  // let completed = true;
+  // if (req.body.completed === 'Отметить как невыполненую') {
+  //   completed = false;
+  // }
+  await Task.updateOne({_id: req.params.id}, {completed: req.params.completed});
+  res.status(200).json('ok');
 });
 //редактирование
-app.get('/tasks/edit/:id', async (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
   const tasks = await Task.find({_id: req.params.id});
   let data;
   tasks.forEach(el => data = el);
   res.render('task_edit', {data});
 });
-app.post('/tasks/edit', async (req, res) => {
-  let completed = true;
-  if (req.body.completed === 'Невыполнена') {
-    completed = false;
-  }
-  await Task.updateOne({_id: req.body._id}, {title: req.body.title, completed: completed});
-  res.redirect('/tasks');
+app.put('/tasks/:id', async (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+  res.status(200).json('ok');
+  // let completed = true;
+  // if (req.body.completed === 'Невыполнена') {
+  //   completed = false;
+  // }
+  // await Task.updateOne({_id: req.body._id}, {title: req.body.title, completed: completed});
+  // res.redirect('/tasks');
 });
 //удаление
-app.post('/tasks/delete', async (req, res) => {
-  console.log(req.body);
-  await Task.findByIdAndRemove({_id: req.body._id});
-  res.redirect('/tasks');
+app.delete('/tasks/:id', async (req, res) => {
+  console.log(req.params.id);
+  await Task.findByIdAndRemove({_id: req.params.id});
+  res.status(204).json('ok');
 });
 //добавление задач
-app.get('/task/add', async (req, res) =>{
+app.get('/tasks/add', async (req, res) =>{
   res.render('add_task');
 });
-app.post('/task/add', async (req, res) => {
+app.post('/tasks', async (req, res) => {
   const {_id} = req.user;
   data_completed = true;
   if (req.body.completed === 'Невыполнена') {
@@ -82,6 +97,7 @@ app.post('/task/add', async (req, res) => {
   }
   const task = new Task({title: req.body.title, completed: data_completed, user: _id});
   task.save();
+  res.status(204);
   res.redirect('/tasks');
 });
 //регистрация
@@ -93,9 +109,13 @@ app.post('/register', async (req, res) => {
   if (restBody.password === repassword) {
     const user = new User(restBody);
     await user.save();
-    res.redirect('/auth');
+    res.redirect('/auth');//с сессией
+
+    // res.status(201);
   } else {
-    res.redirect('/register?err=repass');
+    res.redirect('/register?err=repass');//ссесия
+
+    // res.status(400).json({err: 'ошибка регистрации'});
   }
 });
 // аунтефикация
