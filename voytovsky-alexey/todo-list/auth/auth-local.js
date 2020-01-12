c224826347dec0d50d3b8f6c2f03408dae0ac191
Alex V <al.voytovsky@gmail.com>
2020-01-12 16:10:11 +0300

    Auth: comments added.

--- voytovsky-alexey/todo-list/auth/auth-local.js
+++ voytovsky-alexey/todo-list/auth/auth-local.js
@@ -4,18 +4,20 @@ const User      = require('../users/user-model');
 
 // e-mail & password authentication
 passport.use(
+  // Done is callback function to determine whether user has been successfully authorized
   new Strategy({ usernameField: 'email' }, async (username, password, done) => {
     try {
       const user = await User.findOne({ email: username });
 
       if (!user || !user.validatePassword(password)) {
-        return done(null, false);
+        return done(null, false);  // null is reserved for error handling
       }
 
+      // Converting Mongo object to plain JS object (we need clean object: only fields, no methods)
       const plainUser = JSON.parse(JSON.stringify(user));
+      // Now it's possible to delete password
       delete plainUser.password;
-
-      done(null, plainUser);
+      done(null, plainUser);  // req.user
     }
     catch(err) {
       throw new Error(err);
@@ -24,18 +26,20 @@ passport.use(
   })
 );
 
+// User is plainUser from above here
 passport.serializeUser((user, done) => {
+  // Saving user ID to the session to recognize him (the user) in future
   done(null, user._id);
 });
 
+// This is used when the user comes in future.
 passport.deserializeUser(async (id, done) => {
   try {
     const user = await User.findById(id);
 
     const plainUser = JSON.parse(JSON.stringify(user));
     delete plainUser.password;
-
-    done(null, plainUser);
+    done(null, plainUser);  // req.user
   }
   catch(err) {
     throw new Error(err);
